3ae43a39eee4976fe3cc831370aeb9acf8c69382
songshiqi <403395210@qq.com>
2022-04-06 16:57:39 +0800

    全文搜索排序，面包屑接口，修改项目进度

--- service-client/service-list-client/src/main/java/com/atguigu/gmall1213/list/client/ListFeignClient.java
+++ service-client/service-list-client/src/main/java/com/atguigu/gmall1213/list/client/ListFeignClient.java
@@ -2,13 +2,39 @@ package com.atguigu.gmall1213.list.client;
 
 import com.atguigu.gmall1213.common.result.Result;
 import com.atguigu.gmall1213.list.client.impl.ListDegradeFeignClient;
+import com.atguigu.gmall1213.model.list.SearchParam;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(value="service-list",fallback=ListDegradeFeignClient.class)
 public interface ListFeignClient {
     // 发布接口 热度排名
     @GetMapping("api/list/inner/incrHotScore/{skuId}")
     Result incrHotScore(@PathVariable Long skuId);
+    /**
+     * 搜索商品
+     * @param listParam
+     * @return
+     */
+    @PostMapping("/api/list")
+    Result list(@RequestBody SearchParam listParam);
+
+    /**
+     * 上架商品
+     * @param skuId
+     * @return
+     */
+    @GetMapping("/api/list/inner/upperGoods/{skuId}")
+    Result upperGoods(@PathVariable("skuId") Long skuId);
+
+    /**
+     * 下架商品
+     * @param skuId
+     * @return
+     */
+    @GetMapping("/api/list/inner/lowerGoods/{skuId}")
+    Result lowerGoods(@PathVariable("skuId") Long skuId);
 }
